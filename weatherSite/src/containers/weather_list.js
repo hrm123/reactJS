b85dbd98c55896b8dc647fa520f19083bc454671
bestc0der <hrammohan@yahoo.com>
2016-12-05 07:36:58 -0500

    delta

--- weatherSite/src/containers/weather_list.js
+++ weatherSite/src/containers/weather_list.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { bindActionCreators } from 'redux'; 
+import { Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component {
 
     constructor(props){
+        debugger;
         super(props);
         //this.state =  {term :''};
         //this.onInputChange = this.onInputChange.bind(this); // needed so that this will be SearchBar Component in the call back
         //this.onFormSubmit = this.onFormSubmit.bind(this); 
+    }
+
+    
+
+    renderCity(cityData) {
+        debugger;
+        const cityName = cityData.city.name;
+        const temps = cityData.list.map( weather => weather.main.temp);
+        
+        return (
+            <tr key={cityName}>
+                <td>{cityName} </td>
+                 <td>
+                    <Sparklines height={120} width={180} data={temps}>
+                        <SparklinesLine color="blue" />
+                    </Sparklines>
+                 </td>
+            </tr>
+
+        );
     }
     
     /*
@@ -35,6 +57,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
+                {this.props.weather.map(this.renderCity)}
                 </tbody>
             </table>
         );
