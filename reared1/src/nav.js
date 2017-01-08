dc77cfbb764a0dbb9ab1291a99772d8ae23cd182
RamMohan H <RamMohan H>
2017-01-08 10:24:21 -0500

    init nav added.

--- reared1/src/nav.js
+++ reared1/src/nav.js
@@ -6,10 +6,10 @@ import {Link} from 'react-router';
 class NavCtl extends React.Component{
   render(){
     return (
-      <div>
-        <h1>Home</h1>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">Home</div>
         <Link to="About" className="btn btn-primary btn-lg">Learn More</Link>
-      </div>
+      </nav>
     );
   }
 }
