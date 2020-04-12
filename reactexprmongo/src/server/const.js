267a8645ff5464f7adb92950906d9e0f7d5b4dc5
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2020-04-12 10:13:55 -0400

    express added

---
+++ reactexprmongo/src/server/const.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCJ5EDKB48_E3HzQd8t8qlgyqLpMrEpHTk",
+    authDomain: "mytasks-2df8e.firebaseapp.com",
+    databaseURL: "https://mytasks-2df8e.firebaseio.com",
+    projectId: "mytasks-2df8e",
+    storageBucket: "mytasks-2df8e.appspot.com",
+    messagingSenderId: "294474878483",
+    appId: "1:294474878483:web:9cd67a0acd9a762e8d39e6"
+  };
