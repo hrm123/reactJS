4911f8da3fa9cc6333ab1d0f8c4ec65216fb0a4b
h_r_m <pitechworks@gmail.com>
2025-10-08 16:06:21 -0700

    code

---
+++ elec-app/src/js/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App(){
+    const title = "Hello World"
+    const enhancedTitle = title + ' - React App'
+    return (
+        <h1>{enhancedTitle}</h1>
+    )
+}
--- elec-app/src/js/index.js
+++ elec-app/src/js/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
+import App from './app';
 
 const container = document.getElementById('electron');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(<App  />);
